eba9f4d808ac2713f61462cff12e8c25089b40d9
shubhamr1 <shubhamr1@SMD-0854>
2019-08-23 12:34:25 +0530

    Add project files.

---
+++ AjaxDropdown/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxDropdown.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetCountries()
+        {
+            var countries = new List<String>();
+            countries.Add("India");
+            countries.Add("Austrelia");
+            countries.Add("Russia");
+            return Json(countries, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult GetStates(string country)
+        {
+            var States = new List<string>();
+            if (!string.IsNullOrWhiteSpace(country))
+            {
+                if (country.Equals("Australia"))
+                {
+                    States.Add("Sydney");
+                    States.Add("Perth");
+                }
+                if (country.Equals("India"))
+                {
+                    States.Add("Delhi");
+                    States.Add("Mumbai");
+                }
+                if (country.Equals("Russia"))
+                {
+                    States.Add("Minsk");
+                    States.Add("Moscow");
+                }
+            }
+            return Json(States, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AjaxDropdown/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AjaxDropdown")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AjaxDropdown")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("92a9d182-5966-4fcc-86df-d1977e16e9fc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
